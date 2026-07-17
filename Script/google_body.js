a353b5991c75a23d9f3bbe9c747fcb340e37872a
GelidVeil <hasdied@gmail.com>
2026-07-18 01:19:14 +0800

    更新 google_body.js

--- Script/google_body.js
+++ Script/google_body.js
@@ -1,10 +1,10 @@
 function handleResponse(response) {
     var body = response.body;
     if (body && typeof body === 'string') {
-        var newBody = body;
-        newBody = newBody.replace(/https?:\/\/(www\.)?google\.[a-z]{2,3}/g, 'https://www.google.com');
-        newBody = newBody.replace(/https?:\/\/(www\.)?youtube\.[a-z]{2,3}/g, 'https://www.youtube.com');
-        newBody = newBody.replace(/https?:\/\/(www\.)?gmail\.[a-z]{2,3}/g, 'https://www.gmail.com');
+        var newBody = body
+            .replace(/https?:\/\/(www\.)?google\.[a-z]{2,3}/g, 'https://www.google.com')
+            .replace(/https?:\/\/(www\.)?youtube\.[a-z]{2,3}/g, 'https://www.youtube.com')
+            .replace(/https?:\/\/(www\.)?gmail\.[a-z]{2,3}/g, 'https://www.gmail.com');
         if (newBody !== body) {
             response.body = newBody;
         }
