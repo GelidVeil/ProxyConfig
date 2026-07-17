df7895f4a133900a798955a9b86586032f2723c9
GelidVeil <hasdied@gmail.com>
2026-07-18 01:19:56 +0800

    更新 google_redirect.js

--- Script/google_redirect.js
+++ Script/google_redirect.js
@@ -1,13 +1,13 @@
 function handleResponse(response) {
     var status = response.statusCode;
-    if ([301,302,303,307,308].indexOf(status) !== -1) {
+    if ([301, 302, 303, 307, 308].indexOf(status) !== -1) {
         var location = response.headers['Location'];
         if (location) {
-            var newLocation = location;
-            newLocation = newLocation.replace(/^https?:\/\/(www\.)?google\.[a-z]{2,3}(\/|$)/, 'https://www.google.com/');
-            newLocation = newLocation.replace(/^https?:\/\/(www\.)?youtube\.[a-z]{2,3}(\/|$)/, 'https://www.youtube.com/');
-            newLocation = newLocation.replace(/^https?:\/\/(www\.)?gmail\.[a-z]{2,3}(\/|$)/, 'https://www.gmail.com/');
-            newLocation = newLocation.replace(/^https?:\/\/(www\.)?googleapis\.[a-z]{2,3}(\/|$)/, 'https://www.googleapis.com/');
+            var newLocation = location
+                .replace(/^https?:\/\/(www\.)?google\.[a-z]{2,3}(\/|$)/, 'https://www.google.com/')
+                .replace(/^https?:\/\/(www\.)?youtube\.[a-z]{2,3}(\/|$)/, 'https://www.youtube.com/')
+                .replace(/^https?:\/\/(www\.)?gmail\.[a-z]{2,3}(\/|$)/, 'https://www.gmail.com/')
+                .replace(/^https?:\/\/(www\.)?googleapis\.[a-z]{2,3}(\/|$)/, 'https://www.googleapis.com/');
             if (newLocation !== location) {
                 response.headers['Location'] = newLocation;
             }
