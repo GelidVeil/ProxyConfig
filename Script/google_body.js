42b35e39d59038c5aa50c93adb61c1aa0c9d2234
GelidVeil <hasdied@gmail.com>
2026-07-18 00:08:14 +0800

    创建 google_body.js

---
+++ Script/google_body.js
@@ -0,0 +1,14 @@
+// 页面内容替换：将页面中的地区域名替换为 .com
+function handleResponse(response) {
+    var body = response.body;
+    if (body && typeof body === 'string') {
+        var newBody = body;
+        newBody = newBody.replace(/https?:\/\/(www\.)?google\.[a-z]{2,3}/g, 'https://www.google.com');
+        newBody = newBody.replace(/https?:\/\/(www\.)?youtube\.[a-z]{2,3}/g, 'https://www.youtube.com');
+        newBody = newBody.replace(/https?:\/\/(www\.)?gmail\.[a-z]{2,3}/g, 'https://www.gmail.com');
+        if (newBody !== body) {
+            response.body = newBody;
+        }
+    }
+    return response;
+}
